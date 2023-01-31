e2bd7a2f3c6b3dd2360a5ddc848a29720ada9b21
Alex Isaak <gafar@xaker.ru>
2023-01-31 20:50:16 +0300

    Add numbers

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int numberA = 31;
+int numberB = 5;
+Console.WriteLine(numberA + numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
